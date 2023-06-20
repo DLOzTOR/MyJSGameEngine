281a6bfd644f0456912462537c320db9f2276a5e
DLOzTOR <zafabar1@gmail.com>
2023-06-20 16:31:46 +0300

    add vector2

--- Source/main.js
+++ Source/main.js
@@ -1,4 +1,5 @@
 import { Canvas } from "./Canvas.js";
+import { Vector2 } from "./Math/Vector2.js";
 let canvas = new Canvas(16, 9, 5);
 let colors = ["blue", "red", "green", "yellow", "violet"];
 for(let i = 0; i < 5; i++){
@@ -6,3 +7,11 @@ for(let i = 0; i < 5; i++){
     elem.setAttribute("style", `width: 40%; height: 40%; top: ${15 + 5 * i}%; left: ${10 + 10 * i}%; background-color: ${colors[i]};`);
     canvas.AddElementToLeyer(elem, i);
 }
+
+let myVec = Vector2.One;
+console.log(myVec.ToSting());
+console.log(Vector2.Dot(Vector2.Up, Vector2.One));
+console.log(Vector2.Angle(Vector2.One, Vector2.Up));
+console.log(Vector2.Angle(Vector2.Up, Vector2.Zero));
+console.log(Vector2.LineNormal(Vector2.Zero, Vector2.One.Scale(5)).ToSting());
+console.log(Vector2.Normilize(myVec).ToSting());
--- build/main.js
+++ build/main.js
@@ -37,6 +37,97 @@
     }
   };
 
+  // source/Math/MathConstants.js
+  var MathConstants = class {
+    static RadianToDegrees = 180 / Math.PI;
+    static DegreesToRadian = Math.PI / 180;
+  };
+
+  // source/Math/Vector2.js
+  var Vector2 = class _Vector2 {
+    constructor(X, Y) {
+      this.X = X;
+      this.Y = Y;
+    }
+    Add(vector) {
+      return _Vector2.Add(this, vector);
+    }
+    Angle(vector) {
+      return _Vector2.Angle(this, vector);
+    }
+    Dot(vector) {
+      return _Vector2.Dot(this, vector);
+    }
+    Magnitude() {
+      return _Vector2.Magnitude(this);
+    }
+    Normilize() {
+      return _Vector2.Normilize(this);
+    }
+    Equals(vector) {
+      return _Vector2.Equals(this, vector);
+    }
+    ToSting() {
+      return `X: ${this.X}; Y: ${this.Y}`;
+    }
+    Scale(number) {
+      return _Vector2.Scale(this, number);
+    }
+    Set(X, Y) {
+      this.X = X;
+      this.Y = Y;
+    }
+    static get Zero() {
+      return new _Vector2(0, 0);
+    }
+    static get One() {
+      return new _Vector2(1, 1);
+    }
+    static get Right() {
+      return new _Vector2(1, 0);
+    }
+    static get Left() {
+      return new _Vector2(-1, 0);
+    }
+    static get Up() {
+      return new _Vector2(0, 1);
+    }
+    static get Down() {
+      return new _Vector2(0, -1);
+    }
+    static Equals(vector1, vector2) {
+      return vector1.X == vector2.X && vector1.Y == vector2.Y;
+    }
+    static Add(vector1, vector2) {
+      return new _Vector2(vector1.X + vector2.X, vector1.Y + vector2.Y);
+    }
+    static Angle(vector1, vector2) {
+      if (!_Vector2.Equals(vector1, _Vector2.Zero) && !_Vector2.Equals(vector1, _Vector2.Zero)) {
+        return Math.acos(vector2.Dot(vector1, vector2) / (vector1.Magnitude() * vector2.Magnitude())) * MathConstants.RadianToDegrees;
+      }
+      return NaN;
+    }
+    static Dot(vector1, vector2) {
+      return vector1.X * vector2.X + vector1.Y * vector2.Y;
+    }
+    static Distance(vector1, vector2) {
+      return Math.sqrt(Math.pow(vector2.X - vector1.X, 2) + Math.pow(vector2.Y - vector1.Y, 2));
+    }
+    static Magnitude(vector) {
+      return Math.sqrt(Math.pow(vector.X, 2) + Math.pow(vector.Y, 2));
+    }
+    static Normilize(vector) {
+      let m = _Vector2.Magnitude(vector);
+      return new _Vector2(vector.X / m, vector.Y / m);
+    }
+    static Scale(vector, number) {
+      return new _Vector2(vector.X * number, vector.Y * number);
+    }
+    static LineNormal(vector1, vector2) {
+      return new _Vector2(-(vector2.X - vector1.X), vector2.Y - vector1.Y);
+    }
+  };
+
   // source/main.js
   var canvas = new Canvas(16, 9, 5);
   var colors = ["blue", "red", "green", "yellow", "violet"];
@@ -45,4 +136,11 @@
     elem.setAttribute("style", `width: 40%; height: 40%; top: ${15 + 5 * i}%; left: ${10 + 10 * i}%; background-color: ${colors[i]};`);
     canvas.AddElementToLeyer(elem, i);
   }
+  var myVec = Vector2.One;
+  console.log(myVec.ToSting());
+  console.log(Vector2.Dot(Vector2.Up, Vector2.One));
+  console.log(Vector2.Angle(Vector2.One, Vector2.Up));
+  console.log(Vector2.Angle(Vector2.Up, Vector2.Zero));
+  console.log(Vector2.LineNormal(Vector2.Zero, Vector2.One.Scale(5)).ToSting());
+  console.log(Vector2.Normilize(myVec).ToSting());
 })();
